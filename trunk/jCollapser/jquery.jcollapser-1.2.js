cbd4b1b91d4c1d49ab91ed5672a8eb0972b4d75e
skinn3r <skinn3r@de018098-b63f-11dd-9d36-1d0c09c95ed7>
2009-03-09 06:47:59 +0000

    Add cookie state check. Enabled state setting at initialization time.

--- trunk/jCollapser/jquery.jcollapser-1.2.js
+++ trunk/jCollapser/jquery.jcollapser-1.2.js
@@ -2,12 +2,34 @@
  * Small plugin to make toggling the visibility of certain parts of a page 
  * easier.
  * 
- * @package Collapser
+ * @package jCollapser
  * @author Peter Halasz <skinner@gmail.com>
  * @license http://www.gnu.org/licenses/gpl-3.0.txt GPL v3.0
  * @copyright (c) 2008, Peter Halasz all rights reserved.
  */
 (function($) {
+	/**
+	 * Sets up the functionality.
+	 * 
+	 * Options
+	 * 
+	 * 'container' : Sets the parent element which contains the one we want to 
+	 *               collapse
+	 *               
+	 * 'target'    : The actual element that will collapse or expand
+	 * 'state'     : The initial state we want it to be in
+	 * 
+	 * <sample>
+	 * var options = {
+	 *     container: '#container',
+	 *     target:    '#collapse_element',
+	 *     state:     'collapsed'
+	 * }
+	 * </sample>
+	 * 
+	 * @access public
+	 * @return void
+	 */
 	$.fn.jcollapser = function(options) {
 		
 		var $this = $(this)[0];
@@ -22,8 +44,34 @@
 		
 		$(settings.container + " > .collapse").bind("click", {}, $.fn.jcollapser.collapse);
 		$(settings.container + " > .expand").bind("click", {}, $.fn.jcollapser.expand);
+		
+		/* State from the cookie*/
+		var $state = $.cookie('collapser_' + settings.target);
+		
+		if($state ==  'collapsed') {
+			$(settings.container + ' > .collapse').css("display","none");
+			$(settings.container + ' > .expand').css("display","block");        
+			$(settings.target).hide();
+		} else if(typeof($state) == 'undefined') {
+			/* 
+			 * If we set the state at init time and no state in the cookie 
+			 * then use the init setting. 
+			 */
+			if(settings.state != '' && settings.state == 'collapsed') {
+				$(settings.container + ' > .collapse').css("display","none");
+				$(settings.container + ' > .expand').css("display","block");        
+				$(settings.target).hide();
+			}
+		}
 	};
 	
+	/**
+	 * Collapse a set of elements and set a cookie so we can remember 
+	 * the state.
+	 *
+	 * @access public
+	 * @return void
+	 */
 	$.fn.jcollapser.collapse = function() {
 		var settings = $(this).parents().get(0).id;
 		settings = $(this).jcollapser.settings[settings];
@@ -34,6 +82,13 @@
 		$.cookie('collapser_' + settings.target, 'collapsed', { path: '/', expires: 365 });
 	}
 	
+	/**
+	 * Expand a set of elements and set a cookie so we can remember 
+	 * the state.
+	 * 
+	 * @access public
+	 * @return void
+	 */
 	$.fn.jcollapser.expand = function() {
 		var settings = $(this).parents().get(0).id;
 		settings = $(this).jcollapser.settings[settings];
@@ -47,6 +102,7 @@
 	
 	$.fn.jcollapser.defaults = {
 			container: '#example',
-			target: '#data'
+			target: '#data',
+			state: ''
 	};
 })(jQuery);
